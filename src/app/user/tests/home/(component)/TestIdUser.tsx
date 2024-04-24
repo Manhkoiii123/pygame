395a9762f65f86b615711ddab8559115c423cf71
Tran Duc Manh <manhtranduc0202@gmail.com>
2024-04-24 15:47:00 +0700

    add loading

--- src/app/user/tests/home/(component)/TestIdUser.tsx
+++ src/app/user/tests/home/(component)/TestIdUser.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { userRequest } from "@/apiRequest/user";
+import Loading from "@/components/views/Loading";
 import { AppContext } from "@/lib/context.wrapper";
 import { TQuestion } from "@/types/user";
 import { useMutation } from "@tanstack/react-query";
@@ -29,44 +30,49 @@ const TestIdUser = ({ userTestId }: { userTestId: string }) => {
   useEffect(() => {
     handleFetchQuestion();
   }, [userTestId]);
-  if (!genQuestion) return null;
   return (
-    <div className=" w-[600px] flex justify-center">
-      <div className="flex flex-col gap-4 items-center justify-center">
-        <div className="w-[400px] h-[400px] border border-1 border-gray-200 rounded-lg">
-          <Image
-            src={"/test1.png"}
-            alt="image"
-            width={300}
-            height={300}
-            className="w-full h-full object-cover"
-          ></Image>
-        </div>
-        <span className="font-semibold text-[32px] leading-[44px] text-primary">
-          {testUser.name}
-        </span>
-        <div className="flex flex-col mr-auto gap-2 w-[600px]">
-          <div className="flex items-center gap-2">
-            <div className="w-[8px] h-[8px] bg-[#333] rounded-full"></div>
-            <span>
-              {genQuestion?.total_question} questions are given in{" "}
-              {genQuestion?.time} seconds.
+    <>
+      {generateQuestionMutation.isPending ? (
+        <Loading></Loading>
+      ) : (
+        <div className=" w-[600px] flex justify-center">
+          <div className="flex flex-col gap-4 items-center justify-center">
+            <div className="w-[400px] h-[400px] border border-1 border-gray-200 rounded-lg">
+              <Image
+                src={"/test1.png"}
+                alt="image"
+                width={300}
+                height={300}
+                className="w-full h-full object-cover"
+              ></Image>
+            </div>
+            <span className="font-semibold text-[32px] leading-[44px] text-primary">
+              {testUser.name}
             </span>
-          </div>
-          <div className="flex items-center gap-2">
-            <div className="w-[8px] h-[8px] bg-[#333] rounded-full flex self-start mt-[8px]"></div>
-            <span>{testUser.description}</span>
-          </div>
-          <div className="flex items-center gap-2">
-            <div className="w-[11px] h-[8px] bg-[#333] rounded-full flex self-start mt-[8px]"></div>
-            <span>
-              Use the keyboard’s Left (&lt;) or Right (&gt;) arrow to answer the
-              question and the Up (^) arrow to skip the question.
-            </span>
+            <div className="flex flex-col mr-auto gap-2 w-[600px]">
+              <div className="flex items-center gap-2">
+                <div className="w-[8px] h-[8px] bg-[#333] rounded-full"></div>
+                <span>
+                  {genQuestion?.total_question} questions are given in{" "}
+                  {genQuestion?.time} seconds.
+                </span>
+              </div>
+              <div className="flex items-center gap-2">
+                <div className="w-[8px] h-[8px] bg-[#333] rounded-full flex self-start mt-[8px]"></div>
+                <span>{testUser.description}</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <div className="w-[11px] h-[8px] bg-[#333] rounded-full flex self-start mt-[8px]"></div>
+                <span>
+                  Use the keyboard’s Left (&lt;) or Right (&gt;) arrow to answer
+                  the question and the Up (^) arrow to skip the question.
+                </span>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
