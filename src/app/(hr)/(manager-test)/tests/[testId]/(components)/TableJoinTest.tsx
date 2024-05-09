632653288f7158249bd7476a8e09c4e68a441b63
Tran Duc Manh <manhtranduc0202@gmail.com>
2024-05-09 14:00:58 +0700

    done note

--- src/app/(hr)/(manager-test)/tests/[testId]/(components)/TableJoinTest.tsx
+++ src/app/(hr)/(manager-test)/tests/[testId]/(components)/TableJoinTest.tsx
@@ -8,6 +8,7 @@ import { listTestRequest } from "@/apiRequest/test";
 import Image from "next/image";
 import Loading from "@/components/views/Loading";
 import * as XLSX from "xlsx";
+import UpdateNote from "@/app/(hr)/(manager-test)/tests/[testId]/(components)/UpdateNote";
 const TableJoinTest = ({
   listTest,
   assId,
@@ -26,6 +27,8 @@ const TableJoinTest = ({
     | []
   >([]);
   const [open, setOpen] = useState(false);
+  const [openNote, setOpenNote] = useState(false);
+  const [idOpenNote, setIdOpenNote] = useState("");
   const [labelSelectOptionSort, setLabelSelectOptionSort] =
     useState("applications");
   const [valueSelectOptionSort, setValueSelectOptionSort] = useState(2);
@@ -128,12 +131,12 @@ const TableJoinTest = ({
               .map((item) => item[0].toUpperCase() + item.slice(1))
               .join(" ");
             // console.log(item[key]);
+            // if(newKey.includes())
             newListCandicate[newKey] = item[key];
           }
           return newListCandicate;
         }
       );
-      console.log("🚀 ~ tmp ~ tmp:", tmp);
       const worksheet = XLSX.utils.json_to_sheet(tmp);
       const workbook = XLSX.utils.book_new();
       XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
@@ -164,7 +167,11 @@ const TableJoinTest = ({
     ],
     queryFn: () => handleFetchCandicate(),
   });
-
+  useEffect(() => {
+    if (openNote === false) {
+      setIdOpenNote("");
+    }
+  }, [openNote]);
   const columns: TableProps<any>["columns"] = [
     {
       title: (
@@ -383,21 +390,62 @@ const TableJoinTest = ({
       dataIndex: "note",
       render: (text, record) => {
         return (
-          <div className="flex justify-center text-base font-medium cursor-pointer text-secondary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
-              />
-            </svg>
+          <div
+            onClick={() => {
+              setIdOpenNote(record.id);
+            }}
+            className={`flex justify-center text-base font-medium cursor-pointer  relative ${
+              record.id === idOpenNote ? "text-secondary" : "text-primary"
+            }`}
+          >
+            {record.note !== "" ? (
+              <div
+                onClick={() => {
+                  setOpenNote(!openNote);
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12 2.25c-2.429 0-4.817.178-7.152.521C2.87 3.061 1.5 4.795 1.5 6.741v6.018c0 1.946 1.37 3.68 3.348 3.97.877.129 1.761.234 2.652.316V21a.75.75 0 001.28.53l4.184-4.183a.39.39 0 01.266-.112c2.006-.05 3.982-.22 5.922-.506 1.978-.29 3.348-2.023 3.348-3.97V6.741c0-1.947-1.37-3.68-3.348-3.97A49.145 49.145 0 0012 2.25zM8.25 8.625a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zm2.625 1.125a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875-1.125a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            ) : (
+              <div
+                onClick={() => {
+                  setOpenNote(!openNote);
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+                  />
+                </svg>
+              </div>
+            )}
+            {idOpenNote === record.id && openNote && (
+              <UpdateNote
+                setOpenNote={setOpenNote}
+                id={record.id}
+                oldNote={record.note as string}
+              ></UpdateNote>
+            )}
           </div>
         );
       },
@@ -510,6 +558,7 @@ const TableJoinTest = ({
           columns={columns}
           dataSource={listCandicate}
           pagination={false}
+          className="tbale-custom"
         />
       )}
       {(!isLoading && !listCandicate) ||
---
+++ src/app/(hr)/(manager-test)/tests/[testId]/(components)/UpdateNote.tsx
@@ -0,0 +1,101 @@
+"use client";
+import { listTestRequest } from "@/apiRequest/test";
+import Loading from "@/components/views/Loading";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import React, { useEffect, useRef, useState } from "react";
+import { toast } from "react-toastify";
+type TData = {
+  assessment_candidate_id: number;
+  grading?: number;
+  hiring_stage?: number;
+  note?: string;
+};
+const UpdateNote = ({
+  id,
+  setOpenNote,
+  oldNote,
+}: {
+  id: number;
+  setOpenNote: React.Dispatch<React.SetStateAction<boolean>>;
+  oldNote: string;
+}) => {
+  // const [note, setNote] = useState(oldNote);
+  const textRef = useRef<HTMLTextAreaElement>(null);
+  useEffect(() => {
+    textRef.current!.value = oldNote;
+  }, []);
+  const updateFeedbackReq = async (data: TData) => {
+    const res = await listTestRequest.updateFeedback(data);
+    return res;
+  };
+  const queryClient = useQueryClient();
+  const updateFeedbackMutation = useMutation({
+    mutationFn: (data: TData) => updateFeedbackReq(data),
+  });
+  const saveNote = () => {
+    const note = (textRef?.current)!.value;
+    const data = {
+      assessment_candidate_id: id,
+      note: note,
+    };
+    updateFeedbackMutation.mutate(data, {
+      onSuccess: (res) => {
+        setOpenNote(false);
+        toast.success(res.data.message);
+        textRef.current!.value = "";
+        queryClient.invalidateQueries({
+          queryKey: ["listCandicate"],
+        });
+      },
+    });
+  };
+  const handleCancel = () => {
+    setOpenNote(false);
+    (textRef?.current)!.value = oldNote;
+  };
+  return (
+    <div className="absolute cursor-default top-[100%] -right-[50%] z-50 flex flex-col gap-3 bg-white rounded-lg w-[524px] h-[200px] p-3 shadow-[0px_0px_14px_-4px_#0000000D]">
+      <div className="flex items-center gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 text-primary"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+          />
+        </svg>
+
+        <span className="font-medium text-base text-primary">Leave a note</span>
+      </div>
+      <div className="h-[200px]">
+        <textarea
+          ref={textRef}
+          placeholder="Write your note here"
+          className="w-full text-primary border border-slate-200 rounded-lg py-3 px-5 outline-none bg-transparent h-full resize-none"
+        />
+      </div>
+      <div className="flex gap-2 items-center justify-between">
+        <div
+          onClick={handleCancel}
+          className=" bg-[#CCEBF2] rounded-lg flex-1 flex items-center justify-center py-2 text-secondary cursor-pointer"
+        >
+          Cancel
+        </div>
+        <div
+          onClick={saveNote}
+          className="bg-primary text-white rounded-lg flex-1 flex items-center justify-center py-2 cursor-pointer "
+        >
+          {updateFeedbackMutation.isPending ? <Loading /> : <>Save</>}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UpdateNote;
