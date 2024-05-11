c1a2fafcb70d53a31efe3cca856dd367c91a2989
Tran Duc Manh <manhtranduc0202@gmail.com>
2024-05-11 16:35:43 +0700

    fixbug ui

--- src/app/user/tests/home/[userTestId]/question/(component)/ContentQuestion/NumericalChallenge.tsx
+++ src/app/user/tests/home/[userTestId]/question/(component)/ContentQuestion/NumericalChallenge.tsx
@@ -10,7 +10,7 @@ const NumericalChallenge = () => {
   return (
     <>
       <div className="flex flex-col gap-4">
-        <span className="font-medium text-xl text-primary">
+        <span className="font-medium text-xl text-primary flex items-center justify-center">
           Choose the number that is closer to the right answer.
         </span>
         <div className="p-4 bg-gradient-back-question flex items-center justify-center shadow-[0px_4px_10px_0px_#33B1CB_inset] border-t-4 border-l-4 border-r-4 border-b-1 border-[#33B1CB] rounded-2xl !relative">
